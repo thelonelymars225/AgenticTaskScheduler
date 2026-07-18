274d5a3a83a89ed73fce7066de21f324986d148a
thelonelymars225 <alshmmry923@gmail.com>
2026-07-18 08:29:58 +0300

    Clean generated benchmark dependencies and scratch files

--- example.js
+++
@@ -1,9 +0,0 @@
-function greet(name) {
-    return `Hello, ${name}!`;
-}
-
-const numbers = [1, 2, 3, 4, 5];
-const doubled = numbers.map(n => n * 2);
-
-console.log(greet("world"));
-console.log("Doubled:", doubled);
